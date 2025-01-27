64a4d12ae6be3c17930a0fcd3d0ce9c92e9597ae
kitty14956590 <borbtrash@gmail.com>
2025-01-27 02:25:59 -0500

    fix everything

--- tests/src/generator.c
+++ tests/src/generator.c
@@ -9,12 +9,12 @@ int dump(linked_t * node, void * pass) {
 
 // generate 10 nodes
 int i = 0;
-int computer(linked_t * node, void * pass) {
+int generate(linked_t * node, void * pass) {
 	node->p = (void *) ++i;
 	return i == 11; // stop at node 11
 }
 
 int main() {
-	linked_t * list = linked_compute(NULL, computer, NULL);
+	linked_t * list = linked_generate(NULL, generate, NULL);
 	linked_iterate(list, dump, NULL);
 }
