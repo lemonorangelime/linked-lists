bfb87e712765effe841bf492275c5b07c4325b8c
kitty14956590 <borbtrash@gmail.com>
2025-01-27 02:17:31 -0500

    strip tests/src/computer.c

--- tests/src/computer.c
+++ tests/src/computer.c
@@ -7,12 +7,6 @@ int dump(linked_t * node, void * pass) {
 	printf("- %p <= %p(%p) => %p\n", node->back, node, node->p, node->next);
 }
 
-// filter out odd nodes
-int discriminator(linked_t * node, void * pass) {
-	uintptr_t address = (uintptr_t) node->p;
-	return (address & 1) != 0;
-}
-
 // generate 10 nodes
 int i = 0;
 int computer(linked_t * node, void * pass) {
@@ -22,10 +16,5 @@ int computer(linked_t * node, void * pass) {
 
 int main() {
 	linked_t * list = linked_compute(NULL, computer, NULL);
-	linked_iterate(list, dump, NULL);
-
-	printf("\nRemoving odd nodes\n\n");
-
-	list = linked_discriminate(list, discriminator, NULL);
 	linked_iterate(list, dump, NULL);
 }
